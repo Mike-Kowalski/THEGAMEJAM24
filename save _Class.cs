09e04bfc07e0dce8bff272f5f93ca43b90f2901c
madinsomiac <119925571+madinsomiac@users.noreply.github.com>
2024-04-23 13:51:59 +0200

    mm

---
+++ save _Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_CSharp
+{
+    [Serializable]
+    public class save__Class
+    {
+        public int collectables_found;
+        public int score;
+        public int current_level;
+        public int level_cleared;
+    }
+}
