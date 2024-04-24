b49c282ce649ed2d6e3b6d535121d854221f2aea
madinsomiac <119925571+madinsomiac@users.noreply.github.com>
2024-04-24 11:20:12 +0200

    collectables

--- Assets/scripts/collectable_script.cs
+++ Assets/scripts/collectable_script.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class collectable_script : MonoBehaviour
 {
+    [SerializeField]
+    public int level;
+    [SerializeField]
+    public bool colleted;
+    [SerializeField]
+   public string fileName;
+    Save_Class enteries = new(0, 0, 0, 0, false, false, false, false, false, false);
     // Start is called before the first frame update
     void Start()
     {
-        
+        enteries = file_handler.readFromJson<Save_Class>(fileName);
+        if(level==1)
+        {
+            colleted = enteries.Collected1;
+        }
+        if (level == 2)
+        {
+            colleted = enteries.Collected2;
+        }
+        if (level == 3)
+        {
+            colleted = enteries.Collected3;
+        }
+        if (level == 4)
+        {
+            colleted = enteries.Collected4;
+        }
+        if (level == 5)
+        {
+            colleted = enteries.Collected5;
+        }
+        if (level == 6)
+        {
+            colleted = enteries.Collected6;
+        }
+        if(colleted==true)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +54,31 @@ public class collectable_script : MonoBehaviour
     {
         if(collision.gameObject.tag=="Player")
         {
+            colleted = true;
+            if (level == 1)
+            {
+                enteries.Collected1=colleted;
+            }
+            if (level == 2)
+            {
+                enteries.Collected1 = colleted;
+            }
+            if (level == 3)
+            {
+                enteries.Collected1 = colleted;
+            }
+            if (level == 4)
+            {
+                enteries.Collected1 = colleted;
+            }
+            if (level == 5)
+            {
+                enteries.Collected1 = colleted;
+            }
+            if (level == 6)
+            {
+                enteries.Collected1 = colleted;
+            }
             Destroy(gameObject);
         }
     }
--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -33,7 +33,7 @@ public class Player_movement : MonoBehaviour
     public int curent_lvl;
     public string[] levels;
     public string fileName;
-    Save_Class enteries = new Save_Class(0,0,0,0);
+    Save_Class enteries = new (0, 0, 0, 0,false, false, false, false, false, false);
     [SerializeField]
     public bool upsideDown;
    
