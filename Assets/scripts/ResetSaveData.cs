64ee6e81697d4df7504308e50a6f66f4079ab8aa
madinsomiac <119925571+madinsomiac@users.noreply.github.com>
2024-04-24 10:36:50 +0200

    reset

--- Assets/scripts/ResetSaveData.cs
+++ Assets/scripts/ResetSaveData.cs
@@ -6,10 +6,18 @@ using UnityEngine.UI;
 public class ResetSaveData : MonoBehaviour
 {
     Button ResetButton;
-
-   
+    Save_Class enteries = new Save_Class(0, 0, 0, 0);
+    [SerializeField]
+    public string fileName;
     public void ResetData()
     {
+        enteries.collectables_found = 0;
+        enteries.current_level = 0;
+        enteries.level_cleared = 0;
+        enteries.score = 0;
+
+        
+        file_handler.saveToJson<Save_Class>(enteries,fileName);
         Debug.Log("You have clicked the reset button!");
     }
    
